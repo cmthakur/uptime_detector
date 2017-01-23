09cd5ee675e3ef03a648cd864977e751f9e708d6
cmthakur <cmthakur.r2s@gmail.com>
2017-01-23 16:36:41 +0545

    Add app

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'sinatra'
+require 'httparty'
+require 'sinatra/base'
+require File.dirname(__FILE__) + '/lib/watch_dog'
+
+# load Directory
+Dir[File.dirname(__FILE__) + '/lib/*.rb'].sort.each {|file| require file}
+
+module SiteUp
+  class App < Sinatra::Base
+    get '/' do
+      apps = UpTimeRobot.new(ENV['api_key']).watch
+      haml :index, locals: { apps: apps }
+    end
+  end
+end
+
---
+++ config.ru
@@ -0,0 +1,4 @@
+Bundler.require
+require File.join(File.dirname(__FILE__), 'app.rb')
+
+run SiteUp::App
